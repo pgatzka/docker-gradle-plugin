d65ff1e823735e70ae6c5ae527820541c57e6da6
Philipp Gatzka <phil.gatzka@gmail.com>
2026-05-08 11:44:14 +0200

    feat: CreateVolumeTask

---
+++ src/main/java/io/github/pgatzka/docker/task/CreateVolumeTask.java
@@ -0,0 +1,47 @@
+package io.github.pgatzka.docker.task;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateVolumeCmd;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.provider.MapProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+
+import java.util.Map;
+
+public abstract class CreateVolumeTask extends DockerTask {
+
+    @Input public abstract Property<String> getVolumeName();
+    @Input public abstract Property<String> getDriver();
+    @Input public abstract MapProperty<String, String> getDriverOpts();
+    @Input public abstract MapProperty<String, String> getLabels();
+
+    @TaskAction
+    public void execute() {
+        run(
+            getDockerService().get().getClient(),
+            getVolumeName().get(),
+            getDriver().get(),
+            getDriverOpts().get(),
+            getLabels().get(),
+            getLogger()
+        );
+    }
+
+    static void run(DockerClient c, String name, String driver,
+                    Map<String, String> opts, Map<String, String> labels, Logger log) {
+        boolean exists = c.listVolumesCmd().exec().getVolumes().stream()
+            .anyMatch(v -> name.equals(v.getName()));
+        if (exists) {
+            log.info("Volume {} already exists", name);
+            return;
+        }
+        log.info("Creating volume {}", name);
+        CreateVolumeCmd cmd = c.createVolumeCmd().withName(name).withDriver(driver);
+        if (!opts.isEmpty())   cmd.withDriverOpts(opts);
+        if (!labels.isEmpty()) cmd.withLabels(labels);
+        cmd.exec();
+        log.info("Created volume {}", name);
+    }
+}
