ccdecb2dc1f148087309005a91f94551fbc03bfc
Philipp Gatzka <phil.gatzka@gmail.com>
2026-05-08 12:06:16 +0200

    feat: RemoveContainerTask (force, preserves volumes)

---
+++ src/main/java/io/github/pgatzka/docker/task/RemoveContainerTask.java
@@ -0,0 +1,30 @@
+package io.github.pgatzka.docker.task;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.exception.NotFoundException;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+
+public abstract class RemoveContainerTask extends DockerTask {
+
+    @Input public abstract Property<String> getContainerName();
+
+    @TaskAction
+    public void execute() {
+        run(getDockerService().get().getClient(), getContainerName().get(), getLogger());
+    }
+
+    static void run(DockerClient c, String name, Logger log) {
+        try {
+            c.inspectContainerCmd(name).exec();
+        } catch (NotFoundException nf) {
+            log.info("Container {} not present; nothing to remove", name);
+            return;
+        }
+        log.info("Removing container {}", name);
+        c.removeContainerCmd(name).withForce(true).withRemoveVolumes(false).exec();
+        log.info("Removed container {}", name);
+    }
+}
---
+++ src/test/java/io/github/pgatzka/docker/task/RemoveContainerTaskTest.java
@@ -0,0 +1,41 @@
+package io.github.pgatzka.docker.task;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.InspectContainerCmd;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.command.RemoveContainerCmd;
+import com.github.dockerjava.api.exception.NotFoundException;
+import org.gradle.api.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class RemoveContainerTaskTest {
+
+    @Test void forceRemovesWithoutRemovingVolumes() {
+        DockerClient c = mock(DockerClient.class);
+        InspectContainerCmd ins = mock(InspectContainerCmd.class);
+        InspectContainerResponse resp = mock(InspectContainerResponse.class);
+        RemoveContainerCmd rm = mock(RemoveContainerCmd.class, RETURNS_SELF);
+
+        when(c.inspectContainerCmd("c")).thenReturn(ins);
+        when(ins.exec()).thenReturn(resp);
+        when(c.removeContainerCmd("c")).thenReturn(rm);
+
+        RemoveContainerTask.run(c, "c", mock(Logger.class));
+
+        verify(rm).withForce(true);
+        verify(rm).withRemoveVolumes(false);
+        verify(rm).exec();
+    }
+
+    @Test void noOpWhenAbsent() {
+        DockerClient c = mock(DockerClient.class);
+        InspectContainerCmd ins = mock(InspectContainerCmd.class);
+        when(c.inspectContainerCmd("c")).thenReturn(ins);
+        when(ins.exec()).thenThrow(new NotFoundException("nope"));
+
+        RemoveContainerTask.run(c, "c", mock(Logger.class));
+        verify(c, never()).removeContainerCmd("c");
+    }
+}
