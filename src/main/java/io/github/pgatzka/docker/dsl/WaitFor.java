5fea2682e20aeebde0f716afd8226061138bcf95
Philipp Gatzka <phil.gatzka@gmail.com>
2026-05-08 11:27:30 +0200

    feat: WaitFor sealed hierarchy (healthcheck/logLine/tcpPort/none)

---
+++ src/main/java/io/github/pgatzka/docker/dsl/WaitFor.java
@@ -0,0 +1,15 @@
+package io.github.pgatzka.docker.dsl;
+
+public sealed interface WaitFor
+        permits WaitFor.Healthcheck, WaitFor.LogLine, WaitFor.TcpPort, WaitFor.None {
+
+    record Healthcheck() implements WaitFor {}
+    record LogLine(String regex) implements WaitFor {}
+    record TcpPort(int port) implements WaitFor {}
+    record None() implements WaitFor {}
+
+    static WaitFor healthcheck() { return new Healthcheck(); }
+    static WaitFor logLine(String regex) { return new LogLine(regex); }
+    static WaitFor tcpPort(int port) { return new TcpPort(port); }
+    static WaitFor none() { return new None(); }
+}
